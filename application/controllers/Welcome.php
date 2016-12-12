9f812963b8c29a85eeeecfaecc04530568577405
Jamshid Hashimi <jamshidhashimi@gmail.com>
2016-12-12 10:27:57 +0000

    Added a new example

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,28 +1,35 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+/**
+ * Class Welcome
+ * @author Jamshid Hashimi
+ * @version 1.0
+ */
+
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+    //Class constructor
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('function');
+    }
+
+    //Default function
 	public function index()
 	{
-	    $this->load->helper('function');
+	    //Adding two CSS files, which are required only in this page
         add_css(array('jamshid.css','hashimi.css'));
+        //Adding a JavaScrip file that is only required in this page
         add_js('jamshid.js');
 		$this->load->view('welcome_message');
 	}
+
+	//Here we are adding codeweekend.css which is required only for this page
+	public function new_headers()
+    {
+        add_css('codeWeekend.css');
+        $this->load->view('welcome_message');
+    }
 }
