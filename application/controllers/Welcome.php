6ed58298d20d5abb7883a9027f1b0bf26c59dc8c
Jamshid Hashimi <jamshidhashimi@gmail.com>
2018-07-04 10:48:29 -0700

    formatting fix

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -17,17 +17,17 @@ class Welcome extends CI_Controller {
     }
 
     //Default function
-	public function index()
-	{
-	    //Adding two CSS files, which are required only in this page
+    public function index()
+    {
+        //Adding two CSS files, which are required only in this page
         add_css(array('jamshid.css','hashimi.css'));
         //Adding a JavaScrip file that is only required in this page
         add_js('jamshid.js');
-		$this->load->view('welcome_message');
-	}
+        $this->load->view('welcome_message');
+    }
 
-	//Here we are adding codeweekend.css which is required only for this page
-	public function new_headers()
+    //Here we are adding codeweekend.css which is required only for this page
+    public function new_headers()
     {
         add_css('codeWeekend.css');
         $this->load->view('welcome_message');
